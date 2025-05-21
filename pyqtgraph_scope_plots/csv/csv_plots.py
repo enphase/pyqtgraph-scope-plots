a51d688ada6f4a3d6a9c08baad420297457c47de
Richard Lin <richardlin@enphaseenergy.com>
2025-05-20 18:08:46 -0700

    Add search as a table mixin (#2)
    With SearchSignalsTable, Ctrl+F on the signal table will bring up a
    search box to search by signal names. Included on the CSV viewer,
    helpful for navigating very big files.

--- pyqtgraph_scope_plots/csv/csv_plots.py
+++ pyqtgraph_scope_plots/csv/csv_plots.py
@@ -20,18 +20,12 @@ import numpy.typing as npt
 import pandas as pd
 import pyqtgraph as pg
 from PySide6.QtGui import QAction, QColor
-from PySide6.QtWidgets import (
-    QWidget,
-    QPushButton,
-    QFileDialog,
-    QMenu,
-    QCheckBox,
-    QVBoxLayout,
-)
+from PySide6.QtWidgets import QWidget, QPushButton, QFileDialog, QMenu, QCheckBox, QVBoxLayout
 
 from ..time_axis import TimeAxisItem
 from ..multi_plot_widget import MultiPlotWidget
 from ..plots_table_widget import PlotsTableWidget
+from ..search_signals_table import SearchSignalsTable
 from ..signals_table import ColorPickerSignalsTable, StatsSignalsTable
 from ..timeshift_signals_table import TimeshiftSignalsTable
 from ..transforms_signal_table import TransformsSignalsTable
@@ -61,6 +55,7 @@ class CsvLoaderPlotsTableWidget(PlotsTableWidget):
         PlotsTableWidget.PlotsTableSignalsTable,
         TransformsSignalsTable,
         TimeshiftSignalsTable,
+        SearchSignalsTable,
         StatsSignalsTable,
     ):
         """Adds a hook for item hide"""

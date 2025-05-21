014e7d122f658c901ad3637db0a63ff9e864dd1d
Richard Lin <richardlin@enphaseenergy.com>
2025-05-20 18:03:51 -0700

    Add searchbar

--- pyqtgraph_scope_plots/__init__.py
+++ pyqtgraph_scope_plots/__init__.py
@@ -22,5 +22,8 @@ from .interactivity_mixins import (
 )
 from .enum_waveform_plotitem import EnumWaveformPlot
 from .plots_table_widget import PlotsTableWidget
+from .timeshift_signals_table import TimeshiftSignalsTable
+from .transforms_signal_table import TransformsSignalsTable
+from .search_signals_table import SearchSignalsTable
 
 from .time_axis import TimeAxisItem

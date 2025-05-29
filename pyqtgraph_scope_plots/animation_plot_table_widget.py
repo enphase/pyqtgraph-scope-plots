e2ac1f20c2d8acff7bd63f74744d662413993288
Richard Lin <richardlin@enphaseenergy.com>
2025-05-29 14:56:22 -0700

    wip

--- pyqtgraph_scope_plots/animation_plot_table_widget.py
+++ pyqtgraph_scope_plots/animation_plot_table_widget.py
@@ -1,4 +1,8 @@
-from PySide6.QtWidgets import QInputDialog, QFileDialog
+from typing import List
+
+from PySide6 import QtGui
+from PySide6.QtWidgets import QInputDialog, QFileDialog, QWidget
+from PIL import Image
 
 from .plots_table_widget import PlotsTableWidget
 
@@ -8,17 +12,14 @@ class AnimationPlotsTableWidget(PlotsTableWidget):
     This function handles the UI flow for generating an animation, but does not provide the controls
     to initiate this flow (which is left up to the subclass to implement)"""
 
-    FRAMES_PER_SEGMENT = 8  # TODO these should be user-configurable
-    MS_PER_FRAME = 100
+    FRAMES_PER_SEGMENT = 4  # TODO these should be user-configurable
+    MS_PER_FRAME = 50
 
-    def _start_animation_ui_flow(self) -> None:
+    def _start_animation_ui_flow(self, default_filename: str = "") -> None:
         region_percentage, ok = QInputDialog().getDouble(
             self, "Animation", "Region percentage per frame", 10, minValue=0, maxValue=100
         )
         if not ok:
-            return
-        filename, filter = QFileDialog.getSaveFileName(self, f"Animation", "", "Animated GIF (*.gif)")
-        if not filename:
             return
 
         if isinstance(self._plots._last_region, tuple):
@@ -33,15 +34,30 @@ class AnimationPlotsTableWidget(PlotsTableWidget):
             restore_full_region = False
         assert full_region[1] > full_region[0], "empty region"
 
-        frames_count = self.FRAMES_PER_SEGMENT * (100 / region_percentage)
+        region_size = full_region[1] - full_region[0]
+        frame_size = region_size * (region_percentage / 100)
+        sliding_region_size = region_size - frame_size
+        frames_count = int(self.FRAMES_PER_SEGMENT * (100 / region_percentage))
 
+        capture_windows: List[QWidget] = [self]
         print(self.size())
-        self.grab()
-
-        with open(filename, "w", newline="") as f:
-            pass
+        images = []
+        for i in range(frames_count):
+            frame_center = full_region[0] + (frame_size / 2) + (i / (frames_count - 1) * sliding_region_size)
+            print(i, frame_center, (frame_center - frame_size / 2, frame_center + frame_size / 2))
+            self._plots._on_region_change(None, (frame_center - frame_size / 2, frame_center + frame_size / 2))
+            QtGui.QGuiApplication.processEvents()
+            image = Image.fromqpixmap(self.grab())
+            images.append(image)
 
         if restore_full_region:
-            self._plots._on_region_change(full_region, None)
+            self._plots._on_region_change(None, full_region)
         else:
             self._plots._on_region_change(None, None)
+
+        filename, filter = QFileDialog.getSaveFileName(
+            self, f"Save Animation", default_filename, "Animated GIF (*.gif)"
+        )
+        if not filename:
+            return
+        images[0].save(filename, save_all=True, append_images=images[1:], duration=self.MS_PER_FRAME, loop=0)

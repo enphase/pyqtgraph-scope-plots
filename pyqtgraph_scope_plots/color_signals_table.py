e9217d714e47ce4be7c85eae860e6d876b476e2a
Richard Lin <richardlin@enphaseenergy.com>
2025-06-16 13:18:41 -0700

    Update color_signals_table.py

--- pyqtgraph_scope_plots/color_signals_table.py
+++ pyqtgraph_scope_plots/color_signals_table.py
@@ -78,8 +78,6 @@ class ColorPickerSignalsTable(ContextMenuSignalsTable):
     This gets sent as a signal, and an upper must handle plumbing the colors through.
     """
 
-    _DATA_MODEL_BASES = [ColorPickerDataStateModel]
-
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
 

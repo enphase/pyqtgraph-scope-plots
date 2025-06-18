194007643bd8cfb3b112bcfcb3b4563ee46b244f
Richard Lin <richardlin@enphaseenergy.com>
2025-06-17 18:25:36 -0700

    save/restore visibility

--- pyqtgraph_scope_plots/visibility_toggle_table.py
+++ pyqtgraph_scope_plots/visibility_toggle_table.py
@@ -11,20 +11,47 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-from typing import List, Any, Set
+from typing import List, Any, Set, Optional
 
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QTableWidgetItem, QHeaderView
+from pydantic import BaseModel
 
 from .multi_plot_widget import MultiPlotWidget
+from .save_restore_model import HasSaveLoadDataConfig, BaseTopModel, DataTopModel
 from .signals_table import SignalsTable
 
 
-class VisibilityPlotWidget(MultiPlotWidget):
+class VisibilityDataStateModel(DataTopModel):
+    hidden: Optional[float] = None
+
+
+class VisibilityPlotWidget(MultiPlotWidget, HasSaveLoadDataConfig):
+    _DATA_MODEL_BASES = [VisibilityDataStateModel]
+
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
 
         self._hidden_data: Set[str] = set()  # set of data traces that are invisible
+
+    def _write_model(self, model: BaseModel) -> None:
+        assert isinstance(model, BaseTopModel)
+        super()._write_model(model)
+        for data_name, data_model in model.data.items():
+            assert isinstance(data_model, VisibilityDataStateModel)
+            data_model.hidden = data_name in self._hidden_data
+
+    def _load_model(self, model: BaseModel) -> None:
+        assert isinstance(model, BaseTopModel)
+        super()._load_model(model)
+        for data_name, data_model in model.data.items():
+            assert isinstance(data_model, VisibilityDataStateModel)
+            if data_model.hidden is not None:
+                if data_model.hidden:
+                    self._hidden_data.add(data_name)
+                else:
+                    if data_name in self._hidden_data:
+                        self._hidden_data.remove(data_name)
 
     def hide_data_items(self, data_items: List[str], hidden: bool = True) -> None:
         if hidden:
@@ -54,6 +81,7 @@ class VisibilityToggleSignalsTable(SignalsTable):
 
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
+        self._plots.sigDataUpdated.connect(self._update_visibility_checkbox)
 
     def _pre_cols(self) -> int:
         self.COL_VISIBILITY = super()._pre_cols()
@@ -68,7 +96,8 @@ class VisibilityToggleSignalsTable(SignalsTable):
 
     def _update(self) -> None:
         super()._update()
-        for row, (_, (_, plot_type)) in enumerate(self._plots._data_items.items()):
+        assert isinstance(self._plots, VisibilityPlotWidget)
+        for row, (data_item, (_, plot_type)) in enumerate(self._plots._data_items.items()):
             item = self.item(row, self.COL_VISIBILITY)
             if plot_type == MultiPlotWidget.PlotType.DEFAULT:
                 item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
@@ -76,6 +105,15 @@ class VisibilityToggleSignalsTable(SignalsTable):
                 item.setFlags(Qt.ItemFlag.ItemIsUserCheckable)  # other plots not disable-able
             item.setCheckState(Qt.CheckState.Checked)
             item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    def _update_visibility_checkbox(self):
+        assert isinstance(self._plots, VisibilityPlotWidget)
+        for row, (data_item, (_, plot_type)) in enumerate(self._plots._data_items.items()):
+            item = self.item(row, self.COL_VISIBILITY)
+            if data_item in self._plots._hidden_data:
+                item.setCheckState(Qt.CheckState.Unchecked)
+            else:
+                item.setCheckState(Qt.CheckState.Checked)
 
     def _on_visibility_toggle(self, item: QTableWidgetItem) -> None:
         if item.column() != self.COL_VISIBILITY:
